1c6d8757d7f3b017469d8dea9e8406daa39f3188
BOTCAHX <94074076+BOTCAHX@users.noreply.github.com>
2022-12-20 17:41:47 +0700

    first commit

---
+++ botcahx.js
@@ -0,0 +1,60 @@
+var pino = require('pino')
+var {
+    default: makeWASocket, 
+    fetchLatestBaileysVersion, 
+    MessageRetryMap, 
+    useMultiFileAuthState, 
+    DisconnectReason, 
+    delay
+} = require('@api/whatsapp')
+const msgRetryCounterMap = MessageRetryMap || { }
+
+const startSock = async() => {
+    const { state, saveCreds } = await useMultiFileAuthState('sessions')
+	const { version, isLatest } = await fetchLatestBaileysVersion()
+	console.log(`using WA v${version.join('.')}, isLatest: ${isLatest}`)
+	
+    const sock = makeWASocket({
+		version,
+		logger: pino({ level: 'silent' }),
+		printQRInTerminal: true,
+		auth: state,
+		markOnlineOnConnect: false
+	})
+	sock.ev.process(
+		async(events) => {
+			if(events['connection.update']) {
+				const update = events['connection.update']
+				const { connection, lastDisconnect } = update
+				if(connection === 'close') {
+					if ((lastDisconnect.error)?.output?.statusCode !== DisconnectReason.loggedOut) {
+						await startSock()
+					} else {
+					    console.log('Connection closed. You are logged out.')
+					}
+				}
+				console.log('connection update', update)
+			}
+			if(events['presence.update']) {
+				await sock.sendPresenceUpdate('unavailable')
+			}
+			if(events['messages.upsert']) {
+			  const upsert = events['messages.upsert']
+			  console.log(JSON.stringify(upsert, '', 2))
+			  for (let msg of upsert.messages) {
+				if (msg.key.remoteJid === 'status@broadcast') return
+              }
+			}
+			if(events['creds.update']) {
+				await saveCreds()
+			}
+
+
+		}
+	)
+
+	return sock
+}
+
+startSock()
+process.on('uncaughtException', console.error)
